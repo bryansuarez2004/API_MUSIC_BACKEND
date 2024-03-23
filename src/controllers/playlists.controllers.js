26db93e7cd14dd8bd67cedc769b4e1b0d8eebba7
bryansuarez2004 <bryansf2004@gmail.com>
2024-03-23 14:26:14 -0500

    rutas para playlists terminadas, falta interaccion con canciones

--- src/controllers/playlists.controllers.js
+++ src/controllers/playlists.controllers.js
@@ -1,10 +1,48 @@
 const catchError = require('../utils/catchError');
 const Playlist = require('../models/Playlist');
+const Track = require('../models/Track');
 
 const getAll = catchError(async(req, res) => {
-    return res.json(/* valor a retornar */)
+    const results = await Playlist.findAll({include:[Track]});
+    return res.json(results);
 });
 
+const createPlaylist = catchError(async(req,res)=>{
+    
+   const {idUser, tracks, name} = req.body
+
+   if(tracks.length === 0) return  res.json({error:'debe haber almenos una cancion para crear una playlist'})
+
+  console.log(tracks);
+   const result = await Playlist.create({name,userId:idUser});
+  res.json(result)
+
+})
+
+const removePlaylist = catchError(async(req,res)=>{
+    
+    const {idUser} = req.body
+  
+           const {id} = req.params
+
+    const result = await Playlist.destroy({where:{id,userId:idUser} });
+ 
+   res.json(result)
+ 
+ })
+
+const getPlaylistWithTracks = catchError(async(req,res)=>{
+    const {id} = req.params
+
+    const result = await Playlist.findOne({where:{id},include:[Track]});
+    if(!result) return res.sendStatus(404);
+
+    res.json(result)
+}) 
+
 module.exports = {
-    getAll
+    getAll,
+    createPlaylist,
+    removePlaylist,
+    getPlaylistWithTracks
 }
--- src/routes/playlist.router.js
+++ src/routes/playlist.router.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const { getAll, createPlaylist, removePlaylist, getPlaylistWithTracks } = require('../controllers/playlists.controllers');
 
 const playlistRouter = express.Router();
 
+playlistRouter.route('/')
+   .get(getAll)
 
+playlistRouter.route('/create')
+    .post(createPlaylist)
+
+playlistRouter.route('/:id/remove')
+    .post(removePlaylist)    
+
+playlistRouter.route('/:id/tracks')    
+    .get(getPlaylistWithTracks)
 
 module.exports = playlistRouter;
